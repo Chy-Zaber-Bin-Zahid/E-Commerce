6824ebdd042cead979203b48115596be4d4d11be
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-11-21 08:53:29 +0600

    product page design done 90%

--- client/src/components/Product Components/ProductDetails.jsx
+++ client/src/components/Product Components/ProductDetails.jsx
@@ -28,16 +28,47 @@ function ProductDetails() {
   }, [userId]);
 
   return (
-    <div>
+    <div className="grid grid-cols-2 py-8">
       {productDetails.length === 0 ? (
-        <h1 className="text-2xl text-center w-full p-36">Loading...</h1>
+        <h1 className="text-2xl text-center w-full p-36 col-span-full">
+          Loading...
+        </h1>
       ) : (
         <>
-          <img
-            src={`http://localhost:3001/images/products/${productDetails.image}`}
-            alt={productDetails.title}
-          />
-          <div>hello</div>
+          <div className="flex justify-center items-center">
+            <img
+              src={`http://localhost:3001/images/products/${productDetails.image}`}
+              alt={productDetails.title}
+              className="w-96"
+            />
+          </div>
+          <div>
+            <div className="pb-6 flex flex-col gap-2">
+              <h1 className="text-2xl text-blue-700">{productDetails.title}</h1>
+              <div className="flex gap-1 justify-start items-center">
+                <img src={`/images/wish.png`} alt="Wishlist" />
+                <h1 className="hover:text-orange-600 transition-all duration-300 cursor-pointer">
+                  Add to wishlist
+                </h1>
+              </div>
+            </div>
+            <div>
+              <h1 className="font-semibold text-lg pb-3">Key Features</h1>
+              <div className="flex flex-col gap-1">
+                <h1>Model: {productDetails.keyFeature.model}</h1>
+                <h1>Resolution: {productDetails.keyFeature.resolution}</h1>
+                <h1>Display: {productDetails.keyFeature.display}</h1>
+                <h1>Ports: {productDetails.keyFeature.ports}</h1>
+                <h1>Features: {productDetails.keyFeature.feature}</h1>
+                <h1 className="text-xl font-semibold pt-3">
+                  Price:{" "}
+                  <span className="text-red-600 ">
+                    {productDetails.price}Tk
+                  </span>
+                </h1>
+              </div>
+            </div>
+          </div>
         </>
       )}
     </div>
--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,11 +1,13 @@
 import ProductDetails from "../components/Product Components/ProductDetails";
 import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
 
 function ProductPage({ logged, setLogged, accountId }) {
   return (
     <div>
       <Navbar setLogged={setLogged} logged={logged} accountId={accountId} />
       <ProductDetails />
+      <Footer />
     </div>
   );
 }
