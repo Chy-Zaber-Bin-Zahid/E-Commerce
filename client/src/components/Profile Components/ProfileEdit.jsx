c496d227b1ae5a737dbb509ef672cad28c60aa84
Chowdhury Zaber Bin Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-07-12 21:35:45 +0600

    Update ProfileEdit.jsx

--- client/src/components/Profile Components/ProfileEdit.jsx
+++ client/src/components/Profile Components/ProfileEdit.jsx
@@ -16,7 +16,7 @@ function ProfileEdit({ setUserInfo, upNotification, setUpNotification }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const result = await axios.post("http://localhost:3001/api/user/change", {
+      const result = await axios.post("https://smart-tech-ec8z.onrender.com/api/user/change", {
         email,
         name,
         telephone,
