eb305a6bf163341089cc21f1e9fb7f5f1e995a0f
Chowdhury Zaber Bin Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-07-12 21:12:34 +0600

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,10 @@ const app = require("./app");
 const connectionDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 app.listen(serverPort, async () => {
   console.log(`server is running at http://localhost:${serverPort}`);
   await connectionDatabase();
